9b83a94aa6f25111b5bc9de08f7897c4c7594d78
pashangshangpo <pashangshangpo@Pro.lan>
2017-11-13 23:56:06 +0800

    react组件按需加载

--- src/Route.js
+++ src/Route.js
@@ -1,22 +1,16 @@
 import React, {Component} from 'react';
 import {HashRouter, Route} from 'react-router-dom';
+import asyncComponent from './js/component/AsyncComponent';
 
-class Home extends Component {
-	render() {
-		return (
-			<div>
-				Home
-			</div>
-		);
-	};
-}
+const Home = asyncComponent(() => import('./js/home/index'));
+const Index = asyncComponent(() => import('./js/index/index'));
 
 export default class extends Component {
 	render() {
 		return (
       <HashRouter>
         <div>
-          <Route exact path="/" render={() => <div>Index</div>} />
+          <Route exact path="/" component={Index} />
           <Route exact path="/home" component={Home} />
         </div>
       </HashRouter>
