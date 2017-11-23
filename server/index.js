1e373e1c0c34b92c807dc3ef61b1457ebb3b88c5
pashangshangpo <pashangshangpo@Pro.lan>
2017-11-24 00:26:13 +0800

    如果已经安装过相关依赖则不执行安装依赖命令

--- server/index.js
+++ server/index.js
@@ -1,4 +1,7 @@
 const execSync = require('child_process').execSync;
+const {join} = require('path');
+const fs = require('fs');
+const {root} = require('../config/config');
 
 // 判断是否存在yarn
 const isYarn = () => {
@@ -10,15 +13,35 @@ const isYarn = () => {
   }
 };
 
+// 判断是否安装过依赖
+const isInstall = () => {
+  const packageJson = require(join(root, 'package.json'));
+  let nodeModule = '';
+  try {
+    nodeModule = fs.readdirSync(join(root, 'node_modules'));
+  }
+  catch (e) {
+    return false;
+  }
+
+  const installPackageName = Object.assign({}, packageJson.dependencies, packageJson.devDependencies);
+
+  return Object.keys(installPackageName).every(packageName => {
+    return nodeModule.some(item => item === packageName);
+  });
+};
+
 // 安装依赖
 const install = async () => {
-  console.log('正在为您安装相关依赖');
+  if (!isInstall()) {
+    console.log('正在为您安装相关依赖');
 
-  if (isYarn()) {
-    execSync('yarn install', {stdio: 'inherit'});
-  }
-  else {
-    execSync('npm install', {stdio: 'inherit'});
+    if (isYarn()) {
+      execSync('yarn install', {stdio: 'inherit'});
+    }
+    else {
+      execSync('npm install', {stdio: 'inherit'});
+    }
   }
 };
 
