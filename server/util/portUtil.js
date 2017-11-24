09883345cd1f39369306d90118feab7d30089008
v_xiaozhihua <v_xiaozhihua@baidu.com>
2017-11-24 13:40:37 +0800

    检查端口是否被占用,如果被占用则使用其他端口

---
+++ server/util/portUtil.js
@@ -0,0 +1,40 @@
+const net = require('net');
+
+/**
+ * 检测端口是否被占用
+ * port: 端口
+ * isTrue: 端口被占用时,是否尝试重新获取一个可用的端口
+ * cb: 回调函数
+ */
+module.exports = isPort = (port, isTrue, cb) => {
+  port = Math.ceil(port);
+  
+  if (typeof isTrue === 'function') {
+    cb = isTrue;
+    isTrue = false;
+  }
+
+  // 创建服务并监听该端口
+  const server = net.createServer().listen(port);
+  
+  // 端口未被占用
+  server.on('listening', () => {
+    server.once('close', () => {
+      cb(port);
+    });
+
+    server.close();
+  });
+  
+  // 端口被占用
+  server.on('error', err => {
+    if (err.code === 'EADDRINUSE') {
+      if (isTrue) {
+        isPort(port + 1, isTrue, cb);
+      }
+      else {
+        cb();
+      }
+    }
+  });
+};
