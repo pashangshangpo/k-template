8c6c329bbc9647af53dc124de0b4b80b273476e1
v_xiaozhihua <v_xiaozhihua@baidu.com>
2017-11-25 20:45:13 +0800

    为生产环境起服务

---
+++ server/common/server.js
@@ -0,0 +1,30 @@
+const http = require('http');
+const openbrowser = require('openbrowser');
+const Koa = require('koa2');
+const app = new Koa();
+const Router = require('koa-router');
+const router = new Router();
+const {getIp} = require('../util/util');
+const mockServer = require('../common/mockServer');
+
+module.exports = (port, rou) => {
+    // 服务
+    let server = http.createServer(app.callback());
+    
+    // 转发请求
+    mockServer(app, server);
+
+    app.use(rou(router).routes());
+
+    server.listen(port, () => {
+        // 获取局域网ip
+        let ip = getIp();
+        let url = `http://${ip}:${port}`;
+
+        console.log(`server => ${url}`);
+        console.log(`See request info => ${url}/debug`);
+
+        // 打开浏览器
+        openbrowser(url);
+    });
+};
